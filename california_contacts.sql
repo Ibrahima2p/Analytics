404fe5c0e74e64d762eb0989b9bed48bffb57ef7
Ibrahima2p <105059258+Ibrahima2p@users.noreply.github.com>
2022-06-20 13:46:51 -0400

    Delete california_contacts.sql

--- california_contacts.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE analytics.california_contacts;
-
-CREATE TABLE analytics.california_contacts AS
-SELECT * FROM ds_salesforce.contact 
-WHERE UPPER(mailingstate) = 'CA' OR UPPER(mailingstate) = 'CALIFORNIA'
-      OR 
-      mailingpostalcode IN (SELECT zip 
-                            FROM ds_referencefiles.state_city_zip 
-                            WHERE state = 'CA');
